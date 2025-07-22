65067dd9507d4f791c44a5b93e964eb389cef7ab
Gabriels <gabriels.kukutis@proton.me>
2025-07-22 13:24:35 +0300

    feat(management): deliverables api

---
+++ src/clients/management/deliverables.ts
@@ -0,0 +1,38 @@
+import type { Management } from "../../generated";
+import { APIClient } from "../../lib/client";
+import type { AuthScheme, ClientConfig, OptionsType } from "../../lib/types";
+
+type ResendCommandsForGameServerOptions = OptionsType<
+  Management.operations["Deliverables_ResendCommandsForGameServer"]
+>;
+
+export class DeliverablesAPI {
+  private client: APIClient<Management.paths>;
+
+  constructor(config?: Partial<ClientConfig>) {
+    this.client = new APIClient({ ...config });
+  }
+
+  setToken(scheme: AuthScheme, token: string): void {
+    this.client.setToken(scheme, token);
+  }
+
+  clearToken(): void {
+    this.client.clearToken();
+  }
+
+  setDefaultHeader(key: string, value: string): void {
+    this.client.setDefaultHeader(key, value);
+  }
+
+  removeDefaultHeader(key: string): void {
+    this.client.removeDefaultHeader(key);
+  }
+
+  async getDataMigrations(options: ResendCommandsForGameServerOptions) {
+    return this.client.post(
+      "/v1/stores/{storeId}/deliverables/{deliverableId}/commands/resend",
+      options,
+    );
+  }
+}
